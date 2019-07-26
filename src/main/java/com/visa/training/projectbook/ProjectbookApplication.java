644221406e73e71a495e688ff15e46e86af435b5
Shakthi <shakthi15july@gmail.com>
2019-07-26 11:22:20 +0000

    commit

---
+++ src/main/java/com/visa/training/projectbook/ProjectbookApplication.java
@@ -0,0 +1,13 @@
+package com.visa.training.projectbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbookApplication.class, args);
+	}
+
+}
